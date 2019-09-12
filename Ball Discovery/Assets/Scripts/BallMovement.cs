f92ac98bfe07320f29138cd6a88581e01e1a6c82
Yuri Weessies <YWAM.Weessies@student.han.nl>
2019-09-12 11:08:45 +0200

    player movement

--- Ball Discovery/Assets/Scripts/BallMovement.cs
+++ Ball Discovery/Assets/Scripts/BallMovement.cs
@@ -6,11 +6,23 @@ public class BallMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public float movement_speed;
-
+    public float Jump;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (Physics.Raycast(transform.position, -Vector3.up, 0.7f))
+            {
+                rb.AddRelativeForce(new Vector3(0, 1, 0) * Jump);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -21,7 +33,7 @@ public class BallMovement : MonoBehaviour
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
 
-            Vector3 movement_force = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 movement_force = new Vector3(moveHorizontal, 0.0f, -moveVertical);
 
             rb.AddForce(movement_force.normalized * (movement_speed * Time.fixedDeltaTime));
         }
