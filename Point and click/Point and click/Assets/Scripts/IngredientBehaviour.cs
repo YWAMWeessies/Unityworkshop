e538c8f49806296739fdced129f417266ca36db7
Thimo <tj.smits@outlook.com>
2019-09-19 10:54:45 +0200

    Ingredient pick up, drop and insert into cauldron

---
+++ Point and click/Point and click/Assets/Scripts/IngredientBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientBehaviour : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag.Equals("Cauldron"))
+        {
+            Destroy(gameObject);
+            Debug.Log(name + " went in the cauldron");
+        }
+    }
+}
