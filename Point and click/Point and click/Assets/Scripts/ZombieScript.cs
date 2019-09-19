2bc537f1470c82afed120434c907c16285745d30
RickGZ <rickgz66@gmail.com>
2019-09-19 10:44:17 +0200

    created zombie prefab

---
+++ Point and click/Point and click/Assets/Scripts/ZombieScript.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ZombieScript : MonoBehaviour
+{
+    public UnityEvent m_LightsTurnedOn = new UnityEvent();
+    public GameObject Cauldron;
+    public float WalkingSpeed = 1f;
+    public float RotationSpeed = 1f;
+    private bool _lightsOn = false;
+    private Vector3 origin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origin = transform.position;
+        m_LightsTurnedOn.AddListener(LightsOnHandler);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Cauldron != null)
+        {
+            Walk();
+        }
+
+        CheckForLightsOn();
+    }
+
+    private void CheckForLightsOn()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            m_LightsTurnedOn.Invoke();
+        }
+    }
+
+    private void Walk()
+    {
+        //move towards or away from the cauldron
+        if(!_lightsOn)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Cauldron.transform.position - transform.position), RotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(origin - transform.position), RotationSpeed * Time.deltaTime);
+        }
+
+        transform.position += transform.forward * Time.deltaTime * WalkingSpeed;
+    }
+
+    private void LightsOnHandler()
+    {
+        _lightsOn = !_lightsOn;
+    }
+}
