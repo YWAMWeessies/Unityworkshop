525910edc7ae682a0626f461209e116879b63802
Thimo <tj.smits@outlook.com>
2019-09-19 10:56:17 +0200

    Update amount of ingredients in cauldron

--- Point and click/Point and click/Assets/Scripts/IngredientBehaviour.cs
+++ Point and click/Point and click/Assets/Scripts/IngredientBehaviour.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class IngredientBehaviour : MonoBehaviour
 {
+    public CauldronSO cauldronVariables;
+
     void OnTriggerEnter(Collider other) 
     {
         if (other.tag.Equals("Cauldron"))
         {
             Destroy(gameObject);
+            cauldronVariables._ingredientsInCauldron++;
             Debug.Log(name + " went in the cauldron");
         }
     }
