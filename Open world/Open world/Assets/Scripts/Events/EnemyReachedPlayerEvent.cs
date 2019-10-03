a820f08505a3f671a582f45cf93af9bf0bc14927
Thimo <tj.smits@outlook.com>
2019-10-03 10:30:05 +0200

    Added enemies, chasing player

---
+++ Open world/Open world/Assets/Scripts/Events/EnemyReachedPlayerEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyReachedPlayerEvent : MonoBehaviour
+{
+
+    private bool isEventRunning = false;
+
+    void OnEnable()
+    {
+        AgentBehaviour.PlayerReached += takeDamage;
+    }
+
+
+    void OnDisable()
+    {
+        AgentBehaviour.PlayerReached -= takeDamage;
+    }
+
+
+    void takeDamage()
+    {
+        if (!isEventRunning)
+        {
+            isEventRunning = true;
+        }
+    }
+}
