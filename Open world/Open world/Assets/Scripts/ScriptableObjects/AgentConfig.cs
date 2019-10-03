b896e4d9bc8847352ea6fe3ae28391515c797add
Thimo <tj.smits@outlook.com>
2019-10-03 11:00:47 +0200

    Agent field of view added

---
+++ Open world/Open world/Assets/Scripts/ScriptableObjects/AgentConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AgentConfig", menuName = "ScriptableObjects/AgentConfig", order = 1)]
+public class AgentConfig : ScriptableObject
+{
+    public float AwarenessRange;
+
+    public float FieldOfViewAngle;
+
+    public float WanderingRadius;
+
+}
