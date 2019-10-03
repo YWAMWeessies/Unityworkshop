37040d06d8f6e990ca6c394cf82f592c2d1ac56a
RickGZ <rickgz66@gmail.com>
2019-10-03 10:30:52 +0200

    created player prefab

---
+++ Open world/Open world/Assets/Scripts/CameraControlScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlScript : MonoBehaviour
+{
+
+    [SerializeField]
+    public float Sensitivity = 1f;
+    [SerializeField]
+    public float Smoothening = 2.0f;
+    // the chacter is the capsule
+    public GameObject Character;
+    // get the incremental value of mouse moving
+    private Vector2 _mouseLook;
+    // smooth the mouse moving
+    private Vector2 _smoothV;
+
+    // Use this for initialization
+    void Start()
+    {
+        Character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(Sensitivity * Smoothening, Sensitivity * Smoothening));
+
+        // the interpolated float result between the two float values
+        _smoothV.x = Mathf.Lerp(_smoothV.x, mouseDelta.x, 1f / Smoothening);
+        _smoothV.y = Mathf.Lerp(_smoothV.y, mouseDelta.y, 1f / Smoothening);
+
+        // incrementally add to the camera look
+        _mouseLook += _smoothV;
+
+        // vector3.right means the x-axis
+        transform.localRotation = Quaternion.AngleAxis(-_mouseLook.y, Vector3.right);
+        Character.transform.localRotation = Quaternion.AngleAxis(_mouseLook.x, Character.transform.up);
+    }
+}
---
+++ Open world/Open world/Assets/Scripts/CharacterControlScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControlScript : MonoBehaviour
+{
+    private CharacterController _characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // turn off the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        _characterController.Move(transform.TransformDirection(moveDirection) * Time.deltaTime);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            // turn on the cursor
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
